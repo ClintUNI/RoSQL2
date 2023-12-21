17f3c8a0be2476cada36fdce6ba4e5ee122d4432
Clint <112812765+ClintUNI@users.noreply.github.com>
2023-12-21 18:38:29 +0100

    Add Privatized Fields and Getter/Setter methods

--- TokenClass.lua
+++ TokenClass.lua
@@ -10,6 +10,37 @@ Types = {
 }
 
 Token = {}
+Token.__index = Token
+
+function Token.new()
+    local self: { [string]: any } = setmetatable({}, Token)
+
+    local content: string = ""
+
+    local type: string = Types.WHITESPACE
+
+    function self:SetType(newType: string)
+        type = Types[newType] or type
+    end
+
+    function self:GetType(): string
+        return type
+    end
+    
+    function self:SetContent(newContent: string)
+        content = newContent
+    end
+
+    function self:AppendToContent(additionalContent: string)
+        content = content .. additionalContent
+    end
+
+    function self:GetContent(): string
+        return content
+    end
+
+    return self
+end
 
 function Token.new()
     local t = {}
