bdf922a45777cc12068e49fbd500fe2d14f7c424
Clint <112812765+ClintUNI@users.noreply.github.com>
2023-12-21 18:36:43 +0100

    Indirect Access to Token Fields

--- Lexer.lua
+++ Lexer.lua
@@ -2,44 +2,53 @@ local Token = require("TokenClass")
 
 Lexer = {}
 
+local function OverToken(currentToken, listTokens)
+	if currentToken:GetType() ~= "WHITESPACE" then
+		table.insert(listTokens, currentToken)
+	end
+	return token.new(), listTokens
+end
+
 function Lexer.ParseCode(code)
 	local allTokens = {}
-	local currentToken = Token:new()
+	local currentToken = token.new()
 
 	for i = 1, #code do
 		local char = code:sub(i, i)
 		if char == "(" or char == ")" or char == ',' then
 			currentToken, allTokens = OverToken(currentToken, allTokens)
-			currentToken:updateType("OPERATOR")
-			currentToken:appendContent(char)
+			currentToken:SetType("OPERATOR")
+			currentToken:AppendToContent(char)
 			currentToken, allTokens = OverToken(currentToken, allTokens)
-		elseif char == '0' or char == '1' or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9" then
-			if currentToken.type == "DOUBLE" then
-				currentToken:appendContent(char)
+		elseif char == '0' or char == '1' or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" 
+        or char == "7" or char == "8" or char == "9" then
+			if currentToken:GetType() == "DOUBLE" then
+				currentToken:AppendToContent(char)
 			else
-				currentToken:updateType("INT")
-				currentToken:appendContent(char)
+				currentToken:SetType("INT")
+				currentToken:AppendToContent(char)
 			end
 		elseif char == '.' and currentToken.type == "INT" then
-			currentToken:updateType("DOUBLE")
-			currentToken:appendContent(char)
+			currentToken:SetType("DOUBLE")
+			currentToken:AppendToContent(char)
 		elseif char == '"' or char == "'" then
-			if currentToken.type == "WHITESPACE" then
-				currentToken:updateType("POSSIBLE_STRING")
-			elseif currentToken.type == "POSSIBLE_STRING" then
-				currentToken:updateType("STRING")
+			if currentToken:GetType() == "WHITESPACE" then
+				currentToken:SetType("POSSIBLE_STRING")
+			elseif currentToken:GetType() == "POSSIBLE_STRING" then
+				currentToken:SetType("STRING")
 				currentToken, allTokens = OverToken(currentToken, allTokens)
 			end
 		elseif char == ' ' then
-			if currentToken.type ~= "POSSIBLE_STRING" then
+			if currentToken:GetType() ~= "POSSIBLE_STRING" then
 				currentToken, allTokens = OverToken(currentToken, allTokens)
 			end
 		else
-			if currentToken.type == "WHITESPACE" or currentToken.type == "INT" or currentToken.type == "DOUBLE" then
+			if currentToken:GetType() == "WHITESPACE" or currentToken:GetType() == "INT"
+            or currentToken:GetType() == "DOUBLE" then
 				currentToken, allTokens = OverToken(currentToken, allTokens)
-				currentToken:updateType("IDENTIFIANT")
+				currentToken:SetType("IDENTIFIANT")
 			end
-			currentToken:appendContent(char)
+			currentToken:AppendToContent(char)
 		end
 	end
 
@@ -47,24 +56,17 @@ function Lexer.ParseCode(code)
 
 	for i = 1, #allTokens do
 		token = allTokens[i]
-		if token.type == "IDENTIFIANT" and (token.content == "true" or token.content == "false") then
-			token:updateType("BOOL")
+		if token:GetType() == "IDENTIFIANT" and (token:GetContent() == "true" or token:GetContent() == "false") then
+			token:SetType("BOOL")
 		end
 	end
 
 	return allTokens
 end
 
-function OverToken(currentToken, listTokens)
-	if currentToken.type ~= "WHITESPACE" then
-		table.insert(listTokens, currentToken)
-	end
-	return Token:new(), listTokens
-end
-
-function Lexer.show(listTokens)
-	for i, token in pairs(listTokens) do
-		print("i: "..i..", "..token.content)
+function Lexer.showTokens(tokensList: {})
+	for i, aToken in pairs(tokensList) do
+		print("i: " .. i .. ", ".. aToken:GetContent())
 	end
 end
 
