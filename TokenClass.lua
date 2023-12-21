3b561aa901d6b11e88206ea270cd60da3221c430
Clint <112812765+ClintUNI@users.noreply.github.com>
2023-12-21 18:40:13 +0100

    Debug uses Indirect Access methods

--- TokenClass.lua
+++ TokenClass.lua
@@ -42,30 +42,9 @@ function Token.new()
     return self
 end
 
-function Token.new()
-    local t = {}
-    t.content = ""
-    t.type = Types.WHITESPACE
-    setmetatable(t, {__index = Token})
-    return t
-end
-
 function Token.debug(self)
-    return "\nTOKEN: "..self.content.."\n"..Types[self.type].."\n"
+    return "\nTOKEN: " .. self:GetContent() .. "\n" .. Types[self:GetType()] .. "\n"
 end
 
-function Token.updateContent(self, newContent)
-    self.content = newContent
-end
-
-function Token.appendContent(self, x)
-    self.content = self.content..x
-end
-
-function Token.updateType(self, newType)
-    if Types[newType] ~= nil then
-        self.type = newType
-    end
-end
 
 return Token
